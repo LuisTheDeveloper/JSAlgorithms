66db95207925f89365bead7d2a46c0013ab8d5ce
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2020-03-28 12:46:25 +0000

    Create hof.js

---
+++ hof.js
@@ -0,0 +1,18 @@
+//hof: High Order Function: takes one or more functions as arguments and/or returns a function as its result.
+
+const twice = (f, v) => f(f(v));
+const add3 = v => v + 3;
+
+console.log(twice(add3, 7));  // Result is 13
+
+// this: 
+//    const twice = (f, v) => f(f(v));
+// is the same as:
+      
+      const add3 = v => v + 3;
+      const twice = (f, v) => f(v);
+      
+      x = twice(add3, 7);   // 10
+      y = twice(add3, x);   // 13
+      
+      console.log(y)  // 13
