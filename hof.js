88630c221879cdd585f40e5c2ea289093a514903
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2020-03-28 17:31:47 +0000

    Update hof.js

--- hof.js
+++ hof.js
@@ -1,5 +1,7 @@
 //hof: High Order Function: takes one or more functions as arguments and/or returns a function as its result.
+// also known as callback functions
 
+//********************************************************************//
 const twice = (f, v) => f(f(v));
 const add3 = v => v + 3;
 
@@ -58,3 +60,102 @@ console.log(result)     // 15
 
 const result2 = host(add)(40, 2)
 console.log(result2)    // 42
+
+//********************************************************************//
+
+let x = function(){
+  console.log("Being called inside x function")
+};
+
+let y = function(callback){
+  console.log("inside y function");
+  callback();
+}
+y(x)
+
+//********************************************************************//
+
+// Make this function as most abstract as possible:
+
+let calc = function(num1, num2, calctype) {
+  if (calctype === "add") {
+    return num1 + num2;
+  } else if (calctype === "mult") {
+    return num1 * num2;
+  }
+};
+var result = calc(2,3,"mult");
+console.log(result);
+
+// Alternative approach:
+let add = function(a,b){
+  return a+b
+};
+
+let multiply = function(a,b){
+  return a*b
+};
+
+const calc = function(num1,num2,callback){
+  if (typeof callback === "function") {
+    return callback(num1, num2)
+  }
+    return "Non Function"
+}
+
+const showNumbers = function(a,b){
+  let result = `here are the two numbers passed: ${a}, ${b}`;
+  return result
+  
+}
+
+//console.log(calc2(2,3,multiply))
+result = calc(2,3,showNumbers);
+console.log(result);
+
+// Another approach:
+
+console.log(calc(2,3,function(a,b) {return console.log(`a= ${a} b=${b}`) }))
+
+//********************************************************************//
+
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const subtract = (a, b) => a - b;
+
+// function that returns a function that returns a function
+function calculator() {
+  function doOperation() {
+    function doAdd() {
+      function increment(){}
+        return increment;
+    }
+    return doAdd;
+  }
+
+  return doOperation;
+}
+
+assert(typeof calculator()() === 'function');
+assert(typeof calculator()()() === 'function');
+
+//********************************************************************//
+
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const subtract = (a, b) => a - b;
+
+const calc = function(operation, operand) {
+ let total = 0;
+  const doOperation = function(operation, operand) {
+    console.log(operand)
+    total = operation(total, operand)
+    return total;
+  }
+  total = doOperation(operation, doOperation(operation,operand))
+  return total;
+}
+
+console.log(calc(add,3))
+
+//********************************************************************//
